642c7d3ffd2b39bd8a79f5100e1e900ac8ff1ff6
yassinefeki <136834317+yassinefeki@users.noreply.github.com>
2023-07-11 21:16:02 +0100

    Add files via upload

---
+++ data1.php
@@ -0,0 +1,29 @@
+<?php
+require_once("conn.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>kbjhbh</h1>
+    <?php
+    /*try {
+        $date="11";
+    $temp=11;
+    $wind=11;
+    $sql="INSERT INTO `weather` (`id`, `date`, `temp`, `wind`) VALUES (NULL, '".$date."', '".$temp."', '".$wind."');";
+    $res=$con->query($sql);
+    } catch (PDOException $e) {
+        echo "3asba";
+    }*/
+    
+    ?>
+</body>
+</html>
